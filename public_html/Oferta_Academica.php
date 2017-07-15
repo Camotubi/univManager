5bd1fd9ac2673e470f14e0473414f558b964f911
Pander64 <pander_64@hotmail.com>
2017-07-15 16:15:12 -0500

    Cambios Login
    se cambio la locación de los css , js ,img a assets.
    Se cambio la tipografia en login y palabras

---
+++ public_html/Oferta_Academica.php
@@ -0,0 +1,55 @@
+<?php 
+  require 'header.php';
+?>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    
+                    <a href="#"> 
+                    <div class="col-md-6">
+                        <div class="card" style="background-image: url(assets/img/generarOA.jpg); background-position: center; ">
+                            <div class="header">
+                                
+                            </div>
+                            
+                            
+                            <div class="content">
+                                
+                                
+                                
+                            </div>
+                        </div>
+                    </div>
+                    </a>
+                    
+                    <a href="#"> 
+                    <div class="col-md-6">
+                        <div class="card" style="background-image: url(assets/img/generarHD.jpg); background-position: center; ">
+                            
+                        </div>
+                    </div>
+                    </a>
+                    
+                    
+                </div>
+                
+                <div class="row">
+                    
+                    <a href="#"> 
+                    <div class="col-md-6">
+                        <div class="card" style="background-image: url(assets/img/generarHM.jpg); background-position: center; ">
+                            
+                        </div>
+                    </div>
+                    </a>
+                    
+                    
+                </div>
+
+            </div>
+        </div>
+
+
+<?php 
+  require 'footer.php';
+?>
---
+++ public_html/footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+            <div class="container-fluid">
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> Grupo 1IF-131, Ingeniería de Sistemas de Información, Universidad Tecnológica de Panamá
+                </p>
+            </div>
+        </footer>
+
+    </div>
+</div>
+
+
+</body>
+
+     <!--   Core JS Files   -->
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+	<!--  Checkbox, Radio & Switch Plugins -->
+	<script src="assets/js/bootstrap-checkbox-radio-switch.js"></script>
+
+	<!--  Charts Plugin -->
+	<script src="assets/js/chartist.min.js"></script>
+
+    <!--  Notifications Plugin    -->
+    <script src="assets/js/bootstrap-notify.js"></script>
+
+    <!--  Google Maps Plugin    -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+    <!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+	<script src="assets/js/light-bootstrap-dashboard.js"></script>
+
+	<script src="assets/js/move.js"></script>
+
+</html>
