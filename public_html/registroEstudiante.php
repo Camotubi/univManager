e5c1304f990c90b5cf0d648a41d01e94e7fb945c
Camotubi <camotubi@gmail.com>
2017-07-16 04:59:55 -0500

    fix some issues with config and security file, also addfunctionality to registroEstudiante (WIP)

--- public_html/registroEstudiante.php
+++ public_html/registroEstudiante.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require '../resources/config.php';
-require '../resources/security.php';
+require "../resources/config.php";
+require "../resources/security.php";
 require "../resources/Persona.php";
 if(isset($_POST["tipoRegistro"])){$tipoRegistro = $_POST["tipoRegistro"];}
 ?>
@@ -34,7 +34,12 @@ echo 'Nombre: <input type="text" name="nombre" required autofocus placeholder="N
 <input type="text" name="cedula" required autofocus placeholder="Cedula del Propietario" title="Introduzca su nombre aqui"><br><br>
 <input type="text" name="email" required autofocus placeholder="Cedula del Propietario" title="Introduzca su nombre aqui"><br><br>
 <input type="text" name="telefono" required autofocus placeholder="Cedula del Propietario" title="Introduzca su nombre aqui"><br><br>
-<input type="text" name="direccion" required autofocus placeholder="Cedula del Propietario" title="Introduzca su nombre aqui"><br><br>';
+<input type="text" name="direccion" required autofocus placeholder="Cedula del Propietario" title="Introduzca su nombre aqui"><br><br>
+<select name="sexo">
+<option value="M">Masculino</option>
+<option value="F">Femenino</option>
+</select>';
+
 					echo '<input type="hidden" name="tipoRegistro" value="Registro Unico">';
 
 					echo'<input type="submit">';
@@ -46,18 +51,18 @@ echo 'Nombre: <input type="text" name="nombre" required autofocus placeholder="N
 			{
 				if(isset($_POST["cantRegistros"]))
 				{
+							$cantRegistros=$_POST["cantRegistros"];
 					if(!isset($_POST["EjecutarRegistroMasivo"]))
 					{
 
 					
 						echo'<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">';
 						echo '<input type="hidden" name="tipoRegistro" value="Registro Masivo">';
-							$cantRegistros=$_POST["cantRegistros"];
 							echo'<table>
 								<tr>
 									<th>Nombre</th><th>Apellido</th><th>Cedula</th><th>Email</th><th>Telefono</th><th>Direccion</th>
 								</tr>';
-							for($i= 1; $i<=$cantRegistros;$i++)
+							for($i= 0; $i<$cantRegistros;$i++)
 							{
 								echo '
 								<tr>
@@ -73,7 +78,7 @@ echo 'Nombre: <input type="text" name="nombre" required autofocus placeholder="N
 										
 									</td>
 									<td>
-										<input type=text name="email'.$i.'" required>	
+										<input type=text name="correo'.$i.'" required>	
 										
 									</td>
 									<td>
@@ -84,27 +89,60 @@ echo 'Nombre: <input type="text" name="nombre" required autofocus placeholder="N
 										
 										<input type=text name="direccion'.$i.'" required>	
 									</td>
-								</tr>';
+
+									<td>
+
+										<select name="sexo'.$i.'">
+										<option value="M">Masculino</option>
+										<option value="F">Femenino</option>
+										</select>
+									</td>
+									</tr>';
 
 							}
 							echo'</table>';
 							echo'<input type="hidden" name="cantRegistros" value='.$cantRegistros.'>';
-							echo'<input type="hidden" >';
-							echo'<input type="submit",name="EjecutarRegistroMasivo">'; 
+							echo'<input type="hidden" name="EjecutarRegistroMasivo" value="go" >';
+							echo'<input type="submit">'; 
 
 							echo '</form>';
 					}
 					else
 					{
-
+						$estudiante=array();
+						for($i=0;$i<$cantRegistros;$i++)
+						{
+							array_push($estudiante,new Persona($_POST['nombre'.$i],$_POST['apellido'.$i],$_POST['telefono'.$i],$_POST['cedula'.$i],$_POST['direccion'.$i],$_POST['correo'.$i],$_POST['sexo'.$i]));
+						}
+						
+						echo$estudiante[1]->getNombre();	
 						try
 						{
+							$db=$config["db"]["univManager"];
 							$con = new PDO('mysql:host='.$db['host'].';'.'dbname='.$db['dbname'],$db['username'],$db['password']);
-							$stmt = $con->prepare('SELECT username, contra FROM Usuario WHERE username = :username AND contra = :contra');
+							for($i =0 ; $i<$cantRegistros;$i++)
+							{
 
-							$stmt->execute(['username'=>$username,'contra'=>$contra]);
-							
-							$user=$stmt->fetch(PDO::FETCH_ASSOC);
+								$stmt = $con->prepare('SELECT cedula FROM Persona WHERE cedula = :cedula');
+								$stmt->execute(['cedula'=>$estudiante[$i]]->getCedula());
+								$row=$stmt->fetch(PDO::FETCH_ASSOC);
+								if($row->rowCount()<=0)
+								{
+
+									$stmt=$con->prepare('INSERT INTO  Persona(cedula,nombre,apellido,correo,direccion,sexo,telefono) VALUES(:cedula,:nombre,:apellido,:direccion,:sexo,:telefono);INSERT INTO Estudiante(cedula)VALUES(:cedula);');
+									$stmt->execute(['cedula'=>$estudiante[$i]->getCedula(),'nombre'=>$estudiante[$i]->getNombre(),'apellido'=>$estudiante[$i]->getApellido(),'correo'=>$estudiante[$i]->getCorreo(),'direccion'=>$estudiante[$i]->getDireccion(),'sexo'=>$estudiante[$i]->getSexo()]);
+								}
+								else
+								{
+									$stmt = $con->prepare('SELECT * FROM Persona WHERE cedula = :cedula AND nombre =:nombre AND apellido =:apellido AND direccion = :direccion AND sexo =:sexo AND correo=:correo AND telefono=:telefono');
+									$stmt->execute(['cedula'=>$estudiante[$i]->getCedula(),'nombre'=>$estudiante[$i]->getNombre(),'apellido'=>$estudiante[$i]->getApellido(),'correo'=>$estudiante[$i]->getCorreo(),'direccion'=>$estudiante[$i]->getDireccion(),'sexo'=>$estudiante[$i]->getSexo()]);
+									$row=$stmt->fetch(PDO::FETCH_ASSOC);
+									if($row->roWCount()<=0)
+									{
+
+									}
+								}
+							}
 							if($user['username']==$username && $user['contra']=$contra)
 							{
 								session_start();
@@ -138,7 +176,9 @@ echo 'Nombre: <input type="text" name="nombre" required autofocus placeholder="N
 		}
 	}
 ?>
+
 </body>
+</html>
 <?php
 
 
